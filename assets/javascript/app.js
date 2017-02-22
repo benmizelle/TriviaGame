81cc9f08cd7e51fc1f12d0491f762b25d88165cf
Ben Mizelle <Ben Mizelle>
2017-02-22 18:10:10 -0500

    questions/answers put into array, working on timers and displaying to DOM

---
+++ assets/javascript/app.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+var quiz = [{
+  "question": "What year did the Beatles hit the scene?",
+  "choices": ["1950", "1960", "1970"],
+  "correct": "1960"
+}, {
+  "question": "Which of the following is the name of band member?",
+  "choices": ["John Lennon", "Mick Jagger", "Bob Dylan"],
+  "correct": "John Lennon"
+}, {
+  "question": "Which of the following is a song written by the band?",
+  "choices": ["She Loves You", "All Along the Watchtower", "Satisfaction"],
+  "correct": "She Loves You"
+}, {
+  "question": "Which of the following is an Album produced by the band?",
+  "choices": ["Rainbow Bridge", "In the Court of the Crimoson King", "Revolver"],
+  "correct": "Revolver"
+}, {
+  "question": "What was the first song the band performed for an Ameerican audience?",
+  "choices": ["She Loves You", "All My Loving", "Can't Buy Me Love"],
+  "correct": "All My Loving"
+}]
+
+$("#display").getElementById(quiz);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
