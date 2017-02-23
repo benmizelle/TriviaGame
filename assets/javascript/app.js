fe8e051d7b5e512c05c78a0f980e57a6ce4ec2f0
Ben Mizelle <Ben Mizelle>
2017-02-22 19:53:10 -0500

    timer finally works. Now have to get the quiz to show up to DOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,32 @@
 window.onload = function() {
 
+var quiz = [{
+  "question": "What year did the Beatles hit the scene?",
+  "choices": ["1950", "1960", "1970"],
+  "correct": "1960"
+}, {
+  "question": "Which of the following is the name of band member?",
+  "choices": ["John Lennon", "Mick Jagger", "Bob Dylan"],
+  "correct": "John Lennon"
+}, {
+  "question": "Which of the following is a song written by the band?",
+  "choices": ["She Loves You", "All Along the Watchtower", "Satisfaction"],
+  "correct": "She Loves You"
+}, {
+  "question": "Which of the following is an Album produced by the band?",
+  "choices": ["Rainbow Bridge", "In the Court of the Crimoson King", "Revolver"],
+  "correct": "Revolver"
+}, {
+  "question": "What was the first song the band performed for an Ameerican audience?",
+  "choices": ["She Loves You", "All My Loving", "Can't Buy Me Love"],
+  "correct": "All My Loving"
+}]
+
+
+
+
+
+
   $("#start").on("click", stopwatch.start);
 };
 
@@ -9,13 +36,13 @@ var intervalId;
 // Our stopwatch object
 var stopwatch = {
 
-  time: 0,
-  lap: -1,
+  time: 120,
+  lap: 1,
 
   reset: function() {
 
-    stopwatch.time = 0;
-    stopwatch.lap = -1;
+    stopwatch.time = 120;
+    stopwatch.lap = 1;
   },
 
   start: function() {
@@ -54,31 +81,7 @@ var stopwatch = {
     return minutes + ":" + seconds;
   }
 
-var quiz = [{
-  "question": "What year did the Beatles hit the scene?",
-  "choices": ["1950", "1960", "1970"],
-  "correct": "1960"
-}, {
-  "question": "Which of the following is the name of band member?",
-  "choices": ["John Lennon", "Mick Jagger", "Bob Dylan"],
-  "correct": "John Lennon"
-}, {
-  "question": "Which of the following is a song written by the band?",
-  "choices": ["She Loves You", "All Along the Watchtower", "Satisfaction"],
-  "correct": "She Loves You"
-}, {
-  "question": "Which of the following is an Album produced by the band?",
-  "choices": ["Rainbow Bridge", "In the Court of the Crimoson King", "Revolver"],
-  "correct": "Revolver"
-}, {
-  "question": "What was the first song the band performed for an Ameerican audience?",
-  "choices": ["She Loves You", "All My Loving", "Can't Buy Me Love"],
-  "correct": "All My Loving"
-}]
-
-
 };
-
 
 
 
