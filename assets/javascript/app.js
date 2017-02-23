e88ebb7f64c6d268492daf306ff978e7d4deebe4
Ben Mizelle <Ben Mizelle>
2017-02-22 20:52:00 -0500

    quiz displayed but still a ways to go

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,26 @@
 window.onload = function() {
 
+
 var quiz = [{
   "question": "What year did the Beatles hit the scene?",
   "choices": ["1950", "1960", "1970"],
-  "correct": "1960"
+  "correct": 1
 }, {
   "question": "Which of the following is the name of band member?",
   "choices": ["John Lennon", "Mick Jagger", "Bob Dylan"],
-  "correct": "John Lennon"
+  "correct": 0
 }, {
   "question": "Which of the following is a song written by the band?",
   "choices": ["She Loves You", "All Along the Watchtower", "Satisfaction"],
-  "correct": "She Loves You"
+  "correct": 0
 }, {
   "question": "Which of the following is an Album produced by the band?",
   "choices": ["Rainbow Bridge", "In the Court of the Crimoson King", "Revolver"],
-  "correct": "Revolver"
+  "correct": 2
 }, {
   "question": "What was the first song the band performed for an Ameerican audience?",
   "choices": ["She Loves You", "All My Loving", "Can't Buy Me Love"],
-  "correct": "All My Loving"
+  "correct": 1
 }]
 
 
